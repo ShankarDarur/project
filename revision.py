2c00c6c8731fa9acd9f1297f86196dbdf84a17de
Shankar <shankardarur@gmail.com>
2024-03-12 19:08:04 +0530

    Added new Project

---
+++ revision.py
@@ -0,0 +1,14 @@
+x=14
+y=10
+z=5*4*x+2*y-(x+3*4*y)
+print("sum",z)
+
+open('file.txt','a')
+f.write('this sentence is appending in file')
+f.close()
+f=open('file.txt','r')
+print(f.read())
+
+#file.txt --> file_name
+hi
+iam creating a file in the python shell!
